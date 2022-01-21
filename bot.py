db7f712f7826349bb68c08aebe93f5edd8492e31
AJie1210 <curt665566@gmail.com>
2022-01-21 09:28:41 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 from ast import AsyncFunctionDef
+from tokenize import Token
 import discord
 from discord.ext import commands
-import asyncio
+import json
+
+with open('setting.json', mode = 'r', encoding='utf8')as jfile:
+    jdata = json.load(jfile)
+    # with open(file name, mode = r or w..., encoding = 'utf8') as rename 'jfile'
+    # variable 'jdata' = json.load('jfile')
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,14 +20,17 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member: discord.Member):
-    server = bot.get_guild(933732477216899182)
-    channel = bot.get_channel(933785604792131584)
-    await channel.send(f'A member {member.mention} join !')
-    await channel.send(f'Welcome to the {server}')
+    server = bot.get_guild(int(jdata['Guild']))
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
+    await channel.send(f'New member {member.mention} joined !')
+    await channel.send(f'Welcome to the \"{server}\"')
 
 @bot.event
 async def on_member_remove(member: discord.Member):
-    channel = bot.get_channel(933733567601709056)
+    channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(f'Member {member.mention} left')
 
-bot.run('OTMzNjEyMTEzNDY3OTQwOTE3.YekECQ.zeQ0d13I_MUEkELihDsLplx1IZE')
+@bot.command()
+async def ping(ctx):    # ctx include<username, id, server location, channel location>
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+bot.run(jdata['TOKEN'])     # Index = 'TOKEN'
